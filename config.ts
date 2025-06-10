c5817294a6356a488690916de5be762d89cfb15e
oliwier509 <53867161+oliwier509@users.noreply.github.com>
2025-06-10 20:18:21 +0200

    Delete config.ts

--- config.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-    port: process.env.PORT || 3100,
-    socketPort: process.env.PORT || 3000,
-    databaseUrl: process.env.MONGODB_AWS || 'mongodb+srv://admin:@cluster0.8yfhgmb.mongodb.net/IoT?retryWrites=true&w=majority'
-};
